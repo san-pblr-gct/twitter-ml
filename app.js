e99586531e7c9661b27739ffaedc7af8b4cfd7e1
Parthi <parthiban.sudhaman@gmail.com>
2018-10-17 11:02:56 +0530

    Added NLP

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const Twitter = require('twitter');
 const config = require('./config.js');
 var admin = require('firebase-admin');
 const serviceAccount=require('./serviceAccount.json');
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient({
+  projectId: serviceAccount.projectId,
+  keyFilename: './serviceAccount.json'
+})
+
+
+const text = 'Hello, world!';
+
+var document = {
+  content: text,
+  type: 'PLAIN_TEXT',
+};
+
+// Detects the sentiment of the text
+function analyzeSentiment(textToBeAnalyzed) {
+  document.content = textToBeAnalyzed;
+  client
+  .analyzeSentiment({document: {
+    content: textToBeAnalyzed,
+    type: 'PLAIN_TEXT'
+  }})
+  .then(results => {
+    const sentiment = results[0].documentSentiment;
+
+    console.log(`Text: ${textToBeAnalyzed}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+}
+
+
 const T = new Twitter(config);
 
 admin.initializeApp({
@@ -12,8 +47,8 @@ var db = admin.firestore();
 
 // Set up your search parameters
 const params = {
-  q: 'mannargudi',
-  count: 20,
+  q: '#MeToo',
+  count: 1000,
   result_type: 'recent',
   lang: 'en'
 }
@@ -25,6 +60,10 @@ T.get('search/tweets', params, (err, data, response) => {
   }
   // Loop through the returned tweets
   const tweetsId = data.statuses.map(tweet => {
-    db.collection('tweets').doc(tweet.id_str).set(tweet);
-  });   
+    if ((tweet.text != undefined) && (tweet.text.substring(0,2) != 'RT')) {
+      db.collection('tweets').doc(tweet.id_str).set(tweet);
+      analyzeSentiment(tweet.text);
+    }
+  })
+  
 });
